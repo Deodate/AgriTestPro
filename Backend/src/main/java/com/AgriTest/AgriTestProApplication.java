0a4638be3f3640089b63ce7b7d40d9046a00deee
Deodate <ajadeo04@gmail.com>
2025-02-27 23:44:27 +0200

    Configuration

---
+++ Backend/src/main/java/com/AgriTest/AgriTestProApplication.java
@@ -0,0 +1,13 @@
+package com.AgriTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriTestProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriTestProApplication.class, args);
+	}
+
+}
