0bd3a0f2809c009c0a227ede7769a5325afa005d
Deodate <ajadeo04@gmail.com>
2025-07-12 21:48:45 +0200

    Progress

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -324,13 +324,13 @@ const Dashboard = () => {
       <div className="pagination-controls">
         <button onClick={() => setPage(1)} disabled={currentPage === 1}>&laquo;</button>
         <button onClick={() => setPage(currentPage - 1)} disabled={currentPage === 1}>&lt;</button>
-        {Array.from({ length: totalPages }).map(number => (
+        {Array.from({ length: totalPages }).map((_, index) => (
           <button
-            key={number}
-            onClick={() => setPage(number)}
-            className={`pagination-button ${currentPage === number ? 'active' : ''}`}
+            key={index + 1}
+            onClick={() => setPage(index + 1)}
+            className={`pagination-button ${currentPage === index + 1 ? 'active' : ''}`}
           >
-            {number}
+            {index + 1}
           </button>
         ))}
         <button onClick={() => setPage(currentPage + 1)} disabled={currentPage === totalPages}>&gt;</button>
